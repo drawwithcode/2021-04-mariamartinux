8fc9e8935a39407fc0fcc30aefe6aaf3af9ca4db
mariamartinux <mmariaester@gmail.com>
2021-11-08 22:29:28 +0100

    animated dom

---
+++ caught.js
@@ -0,0 +1,28 @@
+let sfondo;
+
+function setup() {
+  sfondo = loadImage("assets/caught.jpg");
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //allButtons.class("my-buttons");
+  background("black");
+  backgroundImage(sfondo);
+  push();
+  textAlign(CENTER);
+  textSize(380);
+  noStroke();
+  fill("red");
+  text("CAUGHT!", width / 2, height / 1.5);
+  pop();
+}
+
+function backgroundImage(img) {
+  push();
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  let scale = Math.max(width / img.width, height / img.height);
+  image(img, 0, 0, (img.width * scale) / 2, (img.height * scale) / 2);
+  pop();
+}
--- sketch.js
+++ sketch.js
@@ -1,13 +1,60 @@
-function preload() {
-  // put preload code here
-}
+const phrase = "CATCH ME IF YOU CAN";
+const words = phrase.split(" ");
+let allButtons;
+let bottone;
+let iterator = 0;
+let sfondo;
+let badge;
 
 function setup() {
+  sfondo = loadImage("assets/catchme.jpeg");
+  badge = loadImage("./assets/fbibadge.png");
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+
+  noCursor();
+
+  //create a button with a single word of the phrase
+  words.forEach((word) => {
+    bottone = createElement("button", word);
+  });
+
+  allButtons = selectAll("button");
+
+  //what happens when you click on a button
+  bottone.mouseClicked(function () {
+    console.log("caught");
+    window.open("caught.html", "_self");
+  });
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  cursor(badge);
+
+  //behaviour of the buttons
+  iterator++;
+  allButtons.forEach((singleButton, i) => {
+    let x = noise((iterator + 40 * i) / 110) * windowWidth;
+    let y = noise((iterator - 40 * i) / 110) * windowHeight;
+    //visual appearance of the buttons
+    singleButton.style("position: absolute");
+    singleButton.style("color: blue");
+    singleButton.style("border-radius: 50px");
+    singleButton.style("background-color: white");
+    singleButton.style("border: 2px solid blue");
+    singleButton.style("font-size: 30px");
+
+    singleButton.position(x, y);
+  });
+
+  backgroundImage(sfondo);
+}
+
+//mantain proportions of the background image
+function backgroundImage(img) {
+  push();
+  translate(width / 2, height / 2);
+  imageMode(CENTER);
+  let scale = Math.max(width / img.width, height / img.height);
+  image(img, 0, 0, img.width * scale, img.height * scale);
+  pop();
 }
