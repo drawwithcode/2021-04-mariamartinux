f16316a28437f688be5f80da4f479d8aeb332f34
mariamartinux <mmariaester@gmail.com>
2021-11-09 16:54:02 +0100

    cambiamenti1

--- sketch.js
+++ sketch.js
@@ -4,14 +4,13 @@ let allButtons;
 let bottone;
 let iterator = 0;
 let sfondo;
-let badge;
+let button1;
+let soundtrack;
 
 function setup() {
   sfondo = loadImage("assets/catchme.jpeg");
-  badge = loadImage("./assets/fbibadge.png");
+  soundtrack = loadSound("assets/soundtrack.mp3");
   createCanvas(windowWidth, windowHeight);
-
-  noCursor();
 
   //create a button with a single word of the phrase
   words.forEach((word) => {
@@ -21,20 +20,18 @@ function setup() {
   allButtons = selectAll("button");
 
   //what happens when you click on a button
-  bottone.mouseClicked(function () {
+  bottone.mousePressed(function () {
     console.log("caught");
     window.open("caught.html", "_self");
   });
 }
 
 function draw() {
-  cursor(badge);
-
   //behaviour of the buttons
   iterator++;
   allButtons.forEach((singleButton, i) => {
-    let x = noise((iterator + 40 * i) / 110) * windowWidth;
-    let y = noise((iterator - 40 * i) / 110) * windowHeight;
+    let x = noise((iterator + 40 * i) / 100) * windowWidth;
+    let y = noise((iterator - 40 * i) / 100) * windowHeight;
     //visual appearance of the buttons
     singleButton.style("position: absolute");
     singleButton.style("color: blue");
@@ -47,6 +44,75 @@ function draw() {
   });
 
   backgroundImage(sfondo);
+
+  push();
+  textAlign(CENTER);
+  textSize(24);
+  fill("white");
+  text(
+    "click on the escaping words to catch Leonardo di Caprio",
+    width / 2,
+    height / 15
+  );
+  pop();
+
+  push();
+  textAlign(CENTER);
+  textSize(16);
+  fill("white");
+  text(
+    "don't know what I'm talking about? 'Catch me if you can' is a cool movie by Spielberg, watch the trailer          ",
+    width / 2,
+    height / 1.08
+  );
+  pop();
+
+  push();
+  let buttonText1 = "HERE";
+  button1 = createButton(buttonText1);
+  button1.style("background-color", "transparent");
+  button1.style("color", "white");
+  button1.style("cursor", "pointer");
+  button1.style("font-size", "16px");
+  button1.style("padding", "8px 20px 8px 20px");
+  button1.style("border-radius", "15px");
+  button1.style("border-style", "none");
+  button1.style("border-color", "transparent");
+  button1.style("border-width", "0px");
+  button1.style("text-decoration", "underline");
+  button1.position(windowWidth / 1.45, windowHeight / 1.115);
+  pop();
+
+  button1.mousePressed(function () {
+    window.open("https://www.youtube.com/watch?v=s-7pyIxz8Qg");
+  });
+
+  // singleButton.style("border-radius: 50px");
+  // singleButton.style("background-color: white");
+  // singleButton.style("border: 2px solid blue");
+  // singleButton.style("font-size: 30px");
+
+  push();
+  let buttonText2 = "PLAY SOUNDTRACK";
+  button2 = createButton(buttonText2);
+  button2.style("background-color", "blue");
+  button2.style("color", "white");
+  button2.style("cursor", "pointer");
+  button2.style("font-size", "16px");
+  button2.style("border-radius", "50px");
+  button2.style("border: 2px solid white");
+
+  button2.position(windowWidth / 1.2, windowHeight / 17);
+  pop();
+
+  button2.mousePressed(function () {
+    if (soundtrack.isPlaying()) {
+      soundtrack.stop();
+      buttonText2 = "STOP";
+    } else {
+      soundtrack.play();
+    }
+  });
 }
 
 //mantain proportions of the background image
@@ -57,4 +123,8 @@ function backgroundImage(img) {
   let scale = Math.max(width / img.width, height / img.height);
   image(img, 0, 0, img.width * scale, img.height * scale);
   pop();
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
